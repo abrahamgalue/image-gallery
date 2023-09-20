8ce811fb8c6188ec4bf5ec253565c9ed20cfe4bc
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-20 16:02:19 -0400

    create firts commit project

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,23 @@
+import fetchImages from '@/lib/fetchImages'
+import type { ImagesResults } from '@/models/Images'
+import ImgContainer from './ImgContainer'
+import addBlurredDataUrls from '@/lib/getBase64'
+
+export default async function Gallery() {
+  const url = 'https://api.pexels.com/v1/curated'
+
+  const images: ImagesResults | undefined = await fetchImages(url)
+
+  if (!images)
+    return <h2 className='m-4 text-2xl font-bold'>No Images Found</h2>
+
+  const photosWithBlur = await addBlurredDataUrls(images)
+
+  return (
+    <section className='px-2 my-3 grid gap-2 grid-cols-gallery'>
+      {photosWithBlur.map(photo => (
+        <ImgContainer key={photo.id} photo={photo} />
+      ))}
+    </section>
+  )
+}
---
+++ src/app/components/ImgContainer.tsx
@@ -0,0 +1,22 @@
+import type { Photo } from '@/models/Images'
+import Image from 'next/image'
+
+type Props = {
+  photo: Photo
+}
+
+export default function ImgContainer({ photo }: Props) {
+  return (
+    <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'>
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
+        placeholder='blur'
+        blurDataURL={photo.blurredDataUrl}
+        className='object-cover group-hover:opacity-75'
+      />
+    </div>
+  )
+}
