50eb0de8efb234a5238e0d5202b38b8fe4f96b27
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-21 12:45:44 -0400

    adding the search bar with its results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Navbar from './components/Navbar'
+
+export const revalidate = 3600
 
 export const metadata: Metadata = {
-  title: 'Next.js Image Gallery',
-  description: 'Net Ninja tutorial series by Dave Gray',
+  title: 'Image Gallery - @abrahamgalue',
+  description: 'A simple image gallery built with Next.js and TailwindCSS',
 }
 
 export default function RootLayout({
@@ -14,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
+        <Navbar />
         <main className='max-w-6xl mx-auto'>{children}</main>
       </body>
     </html>
