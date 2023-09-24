5be59a546ad9de12d67db1f161528f84c115ada6
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-24 02:02:46 -0400

    add favicon and fixed title pages

--- src/app/results/[...myParams]/page.tsx
+++ src/app/results/[...myParams]/page.tsx
@@ -11,8 +11,24 @@ export function generateMetadata({ params: { myParams } }: Props) {
   const page = myParams?.[1] ?? '1'
 
   return {
-    title: `Results for ${topic} - Page ${page}`,
+    title: `${convertString(topic)} | Image Gallery`,
   }
+}
+
+function convertString(str: string): string {
+  // Replace all occurrences of '%20' with a space character
+  let newStr = str.replace(/%20/g, ' ')
+
+  // Split the string into an array of words
+  let words = newStr.split(/[^a-zA-Z0-9]/)
+
+  // Capitalize the first letter of each word and join them back into a string
+  let capitalizedWords = words.map(
+    word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  )
+  let capitalizedStr = capitalizedWords.join(' ')
+
+  return capitalizedStr
 }
 
 export default function SearchResults({ params: { myParams } }: Props) {
